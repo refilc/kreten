bfd84111583c4e8fdf90ded9b94c5dc255134dca
unknown <55nknown@pm.me>
2021-10-02 00:06:27 +0200

    fix offline timetable bug #32

--- lib/models/week.dart
+++ lib/models/week.dart
@@ -7,7 +7,9 @@ class Week {
   Week({required this.start, required this.end});
 
   factory Week.current() {
-    DateTime now = DateTime.now();
+    DateTime _now = DateTime.now();
+    // fix #32
+    DateTime now = DateTime(_now.year, _now.month, _now.day);
     return Week(
       start: now.subtract(Duration(days: now.weekday - 1)),
       end: now.add(Duration(days: DateTime.daysPerWeek - now.weekday)),
